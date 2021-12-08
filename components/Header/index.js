91acc7750282cba5caef654af232f43b35bd3ba9
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-12-08 16:26:56 +0530

    commit code for next js setup

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Header Component</h1>
-        </div>
-    )
-}
+  return (
+    <header className="h-28 bg-gray-700 w-screen text-white flex justify-between items-center px-20">
+      <div id="logo">
+        <h1>Logo</h1>
+      </div>
+      <nav>
+        <ul className="flex">
+          <li className="m-4">Home</li>
+          <li className="m-4">Features</li>
+          <li className="m-4">Portfolio</li>
+          <li className="m-4">Resume</li>
+          <li className="m-4">Clients</li>
+          <li className="m-4">Pricing</li>
+          <li className="m-4">Blog</li>
+          <li className="m-4 mr-0">Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,16 @@
-
 export default function Home() {
   return (
-    <div>
-      <header className="h-28 bg-gray-700 absolute w-screen text-white flex justify-between items-center px-20">
-        <div id="logo">
-          <h1>Logo</h1>
+    <main>
+      <section className="h-screen flex bg-gray-700 text-white p-36">
+        <div className="flex-1 flex flex-col justify-center">
+          <p>WELCOME TO MY WORLD</p>
+          <h1>Hi, I’m Yagnesh Modh</h1>
         </div>
-        <nav>
-          <ul className="flex">
-            <li className="m-4">Home</li>
-            <li className="m-4">Features</li>
-            <li className="m-4">Portfolio</li>
-            <li className="m-4">Resume</li>
-            <li className="m-4">Clients</li>
-            <li className="m-4">Pricing</li>
-            <li className="m-4">Blog</li>
-            <li className="m-4 mr-0">Contact</li>
-          </ul>
-        </nav>  
-      </header>     
-      <main>
-        <section className="h-screen flex bg-gray-700 text-white p-36">
-            <div className="flex-1 flex flex-col justify-center">
-                <p>WELCOME TO MY WORLD</p>
-                <h1>Hi, I’m Yagnesh Modh</h1>
-            </div>
-            <div className="flex-1">
-              <p>WELCOME TO MY WORLD</p>
-                <h1>Hi, I’m Yagnesh Modh</h1>
-            </div>
-          </section>  
-      </main> 
-    </div>
-  )
+        <div className="flex-1">
+          <p>WELCOME TO MY WORLD</p>
+          <h1>Hi, I’m Yagnesh Modh</h1>
+        </div>
+      </section>
+    </main>
+  );
 }
